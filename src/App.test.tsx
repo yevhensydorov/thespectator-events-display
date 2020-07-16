68489c01042b2001f92563eb3de08861331f2dfb
yevhensydorov <assid@ukr.net>
2020-07-16 16:19:17 +0100

    get basic CRA boilerplate

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders heading element", () => {
+  const { getByText } = render(<App />);
+  const headingElement = getByText(/The Spectator events list/);
+  expect(headingElement).toBeInTheDocument();
+});
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Spectator events list</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
